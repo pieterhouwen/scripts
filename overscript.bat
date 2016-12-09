2f62a1ad07b002019db85b82e4b37f07bd534393
Pieter Houwen <pieter_houwen@hotmail.com>
2016-12-09 12:16:27 +0100

    t is een bericht voor de commit

--- overscript.bat
+++ overscript.bat
@@ -4,8 +4,6 @@ title Extreme OverScript
 echo Checking permissions....
 echo Temp >%windir%\tempfile
 if %errorlevel% == 1 then goto perm_fail
-:perm_fail
-echo U heeft niet 
 :main
 echo.
 echo Welkom in de all-in-one batch toolkit!
@@ -14,9 +12,22 @@ echo.
 echo ----------------------------------------------------
 echo -                                                  -
 echo - 1. Windows System Tools                          -
-echo - 2. Hardware information                          -
+echo - 2. Hardware information/tools                    -
 echo - 3. Network Tools                                 -
 echo -                                                  -
 echo ----------------------------------------------------
 set /p choiche=Maak uw keuze:
-if 
+if %choice% == 1 goto winsystools
+if %choice% == 2 goto hwtools
+if %choice% == 3 goto nettools
+
+
+
+
+
+
+
+:perm_fail
+echo U heeft niet de goede permissies, start dit programma opnieuw als admin om het te gebruiken.
+pause >nul
+exit
