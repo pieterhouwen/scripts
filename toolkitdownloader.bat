e00e2a638cf7ce8a4f783f0b1c511c862a1b8c38
Pieter Houwen <pieter_houwen@hotmail.com>
2016-12-12 13:18:39 +0100

    Added downloader

---
+++ toolkitdownloader.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+echo Downloading toolkit, please wait. . .
+mkdir toolkit
+
+bitsadmin /transfer overscript /download /priority normal https://github.com/pieterhouwen/scripts/overscript.bat toolkit/overscript.bat 2>overscript_log.log 1>nul
+
+if %errorlevel% == 1 goto download_error_os
+
+bitsadmin /transfer motherboard /download /priority normal https://github.com/pieterhouwen/scripts/motherboard.bat toolkit/motherboard.bat 2>motherboard_log.log 1>nul
+if %errorlevel% == 1 goto download_error_mb
+bitsadmin /transfer outlookreset /download /priority normal https://github.com/pieterhouwen/scripts/outlookreset.bat toolkit/outlookreset.bat 2>outlookreset_log.log 1>nul
+if %errorlevel% == 1 goto download_error_or
+
+:download_error_os
+:download_error_mb
+:download_error_or
