3161879cb89e589615553056d5bb7cf39fb502b6
Pieter Houwen <pieter_houwen@hotmail.com>
2016-12-13 12:40:30 +0100

    added directory structure for catagorising tools

--- toolkitdownloader.bat
+++ toolkitdownloader.bat
@@ -5,12 +5,12 @@ mkdir toolkit & cd toolkit
 
 bitsadmin /transfer overscript /download /priority normal https://github.com/pieterhouwen/scripts/overscript.bat toolkit/overscript.bat 2>overscript_log.log 1>nul
 
-if %errorlevel% == 1 goto download_error_os
+if %errorlevel% NEQ 0 goto download_error_os
 
 bitsadmin /transfer motherboard /download /priority normal https://github.com/pieterhouwen/scripts/motherboard.bat toolkit/motherboard.bat 2>motherboard_log.log 1>nul
-if %errorlevel% == 1 goto download_error_mb
+if %errorlevel% NEQ 0 goto download_error_mb
 bitsadmin /transfer outlookreset /download /priority normal https://github.com/pieterhouwen/scripts/outlookreset.bat toolkit/outlookreset.bat 2>outlookreset_log.log 1>nul
-if %errorlevel% == 1 goto download_error_or
+if %errorlevel% NEQ 0 goto download_error_or
 
 :download_error_os
 :download_error_mb
