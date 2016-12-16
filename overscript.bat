d28ab60e7fde3ab13a95107e9a772317ba5e71a8
Pieter Houwen <pieter_houwen@hotmail.com>
2016-12-16 12:35:22 +0100

    Added menus

--- overscript.bat
+++ overscript.bat
@@ -3,13 +3,13 @@ cls
 title Extreme OverScript
 echo Checking permissions....
 net session 2>&1 >nul
-if %errorlevel% == 0 then goto perm_success
+if %errorlevel% == 0 goto perm_success
 goto perm_fail
 :perm_success
 :main
+cls
 echo.
-echo Welkom in de all-in-one batch toolkit!
-echo.
+echo       Welkom in de all-in-one batch toolkit!
 echo.
 echo ----------------------------------------------------
 echo -                                                  -
@@ -17,15 +17,50 @@ echo - 1. Windows System Tools                          -
 echo - 2. Hardware information/tools                    -
 echo - 3. Network Tools                                 -
 echo -                                                  -
+echo -           Made by Pieter and Roland              -
+echo -                                                  -
 echo ----------------------------------------------------
-set /p choiche=Maak uw keuze:
+echo.
+set /p choice=Maak uw keuze:
 if %choice% == 1 goto winsystools
 if %choice% == 2 goto hwtools
 if %choice% == 3 goto nettools
 
-:winsystool
+:winsystools
+cls
+echo.
+echo             Windows System Tools menu    
+echo.
+echo ----------------------------------------------------
+echo -                                                  -
+echo - 1. Fast Universal Cleaning Kit                   -
+echo - 2. Legacy bootmenu switcher                      -
+echo - 3. Outlook profile resetter                      -
+echo - 4. Windows 7 Version changer (CD/DVD required)   -
+echo - 5. Show drives and free space                    -
+echo - 6. System File Checker                           -
+echo -                                                  -
+echo -           Made by Pieter and Roland              -
+echo -                                                  -
+echo ----------------------------------------------------
+echo.
+set /p choice=Maak uw keuze:
+
 :hwtools
+cls
+echo.
+echo           Windows Hardware Tools
+echo.
+echo ----------------------------------------------------
+echo -                                                  -
+echo - 1. Get motherboard info                          -
+echo -                                                  -
+echo -           Made by Pieter and Roland              -
+echo -                                                  -
+echo ----------------------------------------------------
 :nettools
+cls
+
 
 REM TODO's
 REM System cleaner van pieterhouwen.info/scripts/cleansys.bat
