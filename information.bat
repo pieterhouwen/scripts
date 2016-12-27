c5a10569077e56dc3281954b4ea3f80206d830c4
Pieter Houwen <pieter_houwen@hotmail.com>
2016-12-27 14:24:55 +0100

    Added multiple language support

--- information.bat
+++ information.bat
@@ -1,6 +1,15 @@
 @echo off
 title Information Finder
 cls
+REM Query installed language:
+
+for /f "tokens=3 delims= " %%L in ('reg query hklm\system\controlset001\control\nls\language /v Installlanguage ^| findstr 0') do set lng=%%L
+if %lng%==0413 set gateway=11
+if %lng%==0409 set gateway=13
+echo %lng%
+echo %gateway%
+pause
+
 echo Gathering info. . . 
 
 REM find OS
@@ -10,15 +19,18 @@ REM find IP
 for /f "tokens=14 delims= " %%B in ('ipconfig -all ^| findstr -i ipv4') do set pieter=%%B
 
 REM find Current gateway
-for /f "tokens=11 delims= " %%A in ('ipconfig -all ^| findstr -i gateway') do set gw=%%A
+for /f "tokens=%gateway% delims= " %%A in ('ipconfig -all ^| findstr -i gateway') do set gw=%%A
+
+for /f "tokens=3 delims= " %%L in ('reg query hklm\system\controlset001\control\nls\language /v Installlanguage') do echo Language: %%L
 
 REM find processor
-for /f "tokens=2 delims==" %%F in ('wmic cpu get name /format:list') do set cpu=%%F
+for /f "tokens=*" %%F in ('wmic cpu get name ^| findstr -i -v name') do echo %%F
 cls
 echo Hello %username%! You are running: %windows%
 echo.
 echo IP: %pieter%		Gateway: %gw%
 echo Processor: %cpu%		
+echo %lng%
 pause
 
 rem Current IP
