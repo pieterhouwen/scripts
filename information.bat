0f29bcb79b3a816ba388bebce24ab75eceac5df5
Pieter Houwen <pieter_houwen@hotmail.com>
2016-12-27 13:31:42 +0100

    Added moar features

--- information.bat
+++ information.bat
@@ -2,25 +2,23 @@
 title Information Finder
 cls
 echo Gathering info. . . 
+
+REM find OS
+for /f "tokens=* delims=" %%E in ('wmic os get caption ^| findstr -i windows') do set windows=%%E
+
 REM find IP
 for /f "tokens=14 delims= " %%B in ('ipconfig -all ^| findstr -i ipv4') do set pieter=%%B
-rem echo B is still %pieter%
 
 REM find Current gateway
 for /f "tokens=11 delims= " %%A in ('ipconfig -all ^| findstr -i gateway') do set gw=%%A
 
-REM find RAM speed
-for /f "tokens=* delims=" %%C in ('wmic Memorychip get speed ^| findstr -i -v speed') do echo %%C
-
-REM find RAM size
-for /f "tokens=* delims=" %%D in ('wmic Memorychip get capacity ^| findstr -i -v capacity') do set rsz=%%D
-
+REM find processor
+for /f "tokens=2 delims==" %%F in ('wmic cpu get name /format:list') do set cpu=%%F
 cls
-echo Hello %username%!
-echo IP: %pieter%		Gateway: %pieter%
-echo RAM: %rsp% Mhz		RAM size: %rsz% bytes
-
-
+echo Hello %username%! You are running: %windows%
+echo.
+echo IP: %pieter%		Gateway: %gw%
+echo Processor: %cpu%		
 pause
 
 rem Current IP
