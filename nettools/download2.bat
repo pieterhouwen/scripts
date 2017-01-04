40ac12be25fc07d4e2cc17951027742ccb17a4f1
Pieter Houwen <pieter@pieterhouwen.info>
2017-01-04 11:28:18 +0100

    Modified filename capture

--- nettools/download2.bat
+++ nettools/download2.bat
@@ -1,14 +1,29 @@
-@echo off
+
 title Downloader
 set name=%1
 set link=%2
-set location=%3
-bitsadmin /transfer %name% /download /priority normal %link% %location%
+rem set location=%3
+set i=0
+set var1=%link%
+:loopprocess
+for /F "tokens=1* delims=/" %%A in ( "%var1%" ) do (
+  set /A i+=1
+  set var1=%%B
+  goto loopprocess )
+
+echo The string contains %i% tokens.
+
+
+for /f "tokens=%i% delims=/" %%F in (%link%) do set url=%%F
+for /f %%a in (%url%) do set location=%%a
+set curdir=%cd%
+:download_bitsadmin
+bitsadmin /transfer %name% /download /priority normal %link% %curdir%\%location%
 if %errorlevel% NEQ 0 goto download_error
-pause
+
 exit
 :download_error
-powershell.exe -command (new-object System.Net.WebClient).DownloadFile('$Env:link', '$Env:temp\ $Env:location')
+powershell.exe -command (new-object System.Net.WebClient).DownloadFile('$Env:link', '$Env:temp\')
 if %errorlevel% NEQ 0 goto download_error_ftp
 pause
 exit
