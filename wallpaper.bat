546c7f24d7f2ec3adaaf8da88d7af68ad82ffff2
Pieter Houwen <pieter_houwen@hotmail.com>
2017-02-15 10:12:37 +0100

    Added right reg import method

--- wallpaper.bat
+++ wallpaper.bat
@@ -10,8 +10,13 @@ goto end
 :mainexecution
 echo Note: All users should be able to view the image or else the background will be empty!
 set /p location=Specify where the image is located (example C:\image.jpg):
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v Wallpaper /t reg_sz /d %location%
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v WallpaperStyle /t reg_sz /d 2
-pause
 
-:end
+:writekey
+echo Windows Registry Editor Version 5.00 >%temp%\wallpaper.reg
+echo. >>%temp%\wallpaper.reg
+echo [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System] >>%temp%\wallpaper.reg
+echo "WallpaperStyle"="2" >>%temp%\wallpaper.reg
+reg import %temp%\wallpaper.reg
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v Wallpaper /d "%location%" /f /t REG_SZ
+echo Done
+pause
