9dc8a5aab257fb754e854a9098e3d869200c6b1c
Pieter Houwen <pieter_houwen@hotmail.com>
2017-03-14 11:48:07 +0100

    Added everything OK message

--- smartmon.bat
+++ smartmon.bat
@@ -1,7 +1,12 @@
+title SMART monitoring tool
 wmic diskdrive get caption, status >%userprofile%\smartmon.log
 findstr -v "OK" %userprofile%\smartmon.log
 if %errorlevel% NEQ 1 goto warning
+goto nowarning
 :warning
-echo msgbox("Your hard drive monitoring system has been triggered! Press OK to learn more.") >%tmp%\alert.vbs
-start alert.vbs
+echo msgbox("Your hard drive monitoring system has been triggered! Press OK to learn more.") >%temp%\alert.vbs
+%temp%\alert.vbs
 cmd.exe /C "wmic diskdrive get caption, status" & pause
+:nowarning
+echo msgbox("All drives report normal.") >%temp%\alert.vbs
+%temp%\alert.vbs
